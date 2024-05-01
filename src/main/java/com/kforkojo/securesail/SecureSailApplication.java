af55dc2e1223b366dafdeffc6d42f00d1db3cd02
Joseph Ampadu <joseph.ampadu@amalitech.com>
2024-05-01 15:26:00 +0000

    Generate spring project

---
+++ src/main/java/com/kforkojo/securesail/SecureSailApplication.java
@@ -0,0 +1,13 @@
+package com.kforkojo.securesail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureSailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kforkojo/securesail/SecureSailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kforkojo.securesail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
